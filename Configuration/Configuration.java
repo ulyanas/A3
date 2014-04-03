632ad90db0b16a9e81d31dcd1d6ad4d01f8f33a7
wangmengyan95 <wangmengyan95@gmail.com>
2014-04-03 15:30:10 -0400

    SystemA

---
+++ Configuration/Configuration.java
@@ -0,0 +1,14 @@
+package Configuration;
+
+public class Configuration {
+	
+	public final  static  int SECURITY_MONITOR_ID  = 8000;
+	
+	public final  static  int SECURITY_CONTROLLER_ID  = 9000;
+	
+	public final  static  int DOOR_SENSOR_ID  = 10000;
+	public final  static  int WINDOW_SENSOR_ID  = 11000;
+	public final  static  int MOTION_SENSOR_ID  = 12000;
+	
+	
+}
